b51d10df560120aff942e93528c4de619ba7d08d
Andrey Gruzdev <andrey.gruzdev@sifox.ru>
2020-09-06 02:13:56 +0300

    added start script

---
+++ add_virtualenv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python_path=$(which python3)
+
+echo "Path to python $python_path"
+
+virtualenv venv -p $python_path
+
+source venv/bin/activate
+
+pip3 install -r requirements.txt
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py createsuperuser
